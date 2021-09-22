01e4d25b2e56096c411904d16fc25fb0d42cbd77
D Dixon <d@themadcalf.com>
2021-09-21 19:46:18 -0700

    Add like button

--- src/components/entry.js
+++ src/components/entry.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as styles from '../styles/entry.module.css';
 
 /* Each item has the following
-	
 copyright: " "
 date:	"2021-09-21"
 explanation:	"... The captured scene, however, is now frozen in time for all to enjoy."
@@ -14,29 +13,38 @@ url	"https://apod.nasa.gov/apod/image/2109/SunSpotHill_Coy_960.jpg"
 */
 
 const Entry = ({ data }) => {
-  console.log('Entry', data);
-  const mediaIsImage = data.mediaType === 'image';
+  const [liked, setLiked] = useState(false);
+  const handleClick = () => {
+    setLiked((liked) => !liked);
+  };
 
   return (
     <article className={styles.entry}>
-      <h3>{data.title}</h3>
-      <p className={styles.date}>{data.date}</p>
-      {data.copyrignt && (
-        <p className={styles.copyright}>Soure: {data.copyright}</p>
-      )}
-      {data.media_type === 'image' ? (
-        <img src={data.url} alt={data.title} />
-      ) : (
-        <div className={styles.videoContainer}>
-          <iframe
-            src={data.url}
-            title={data.title}
-            allow="accelerometer; autoplay; encrypted-media; fullscreen; geolocation; gyroscope; picture-in-picture"
-          />
-        </div>
-      )}
-
-      <p className={styles.explanation}>{data.explanation}</p>
+      <div className={styles.entryContent}>
+        {data.media_type === 'image' ? (
+          <img src={data.url} alt={data.title} />
+        ) : (
+          <div className={styles.videoContainer}>
+            <iframe
+              src={data.url}
+              title={data.title}
+              allow="accelerometer; autoplay; encrypted-media; fullscreen; geolocation; gyroscope; picture-in-picture"
+            />
+          </div>
+        )}
+        <h3>{data.title}</h3>
+        <p className={styles.date}>
+          {data.copyright ? `${data.copyright}: ${data.date}` : `${data.date}`}
+        </p>
+        <p className={styles.explanation}>{data.explanation}</p>
+        <button
+          type="button"
+          onClick={handleClick}
+          className={liked ? styles['liked'] : styles['like']}
+        >
+          {liked ? '😀' : 'Like'}
+        </button>
+      </div>
     </article>
   );
 };
