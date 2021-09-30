8d9db8d1c90e55371d0c4121554e8bcdc07ae69b
D Dixon <d@themadcalf.com>
2021-09-30 12:41:42 -0700

    Save & read liked statuses to/from local storage

--- src/components/entry.js
+++ src/components/entry.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as styles from '../styles/entry.module.css';
+import { storageAvailable } from '../utils/storage';
 
 /* Each item has the following
 copyright: " "
@@ -14,6 +15,24 @@ url	"https://apod.nasa.gov/apod/image/2109/SunSpotHill_Coy_960.jpg"
 
 const Entry = ({ data }) => {
   const [liked, setLiked] = useState(false);
+  const storage = window.localStorage;
+  const { title, url, copyright, explanation, date, media_type } = data;
+
+  // not sure yet how to handle the linter error since i only want this effect to run on mount
+  useEffect(() => {
+    if (storageAvailable) {
+      const stored = JSON.parse(storage.getItem(title));
+      setLiked(stored.liked);
+    } else {
+      setLiked(false);
+    }
+  }, []);
+
+  // Also here, since i only want this effect to run when liked changes, not anything else.
+  useEffect(() => {
+    storage.setItem(title, JSON.stringify({ liked: liked }));
+  }, [liked]);
+
   const handleClick = () => {
     setLiked((liked) => !liked);
   };
@@ -21,22 +40,22 @@ const Entry = ({ data }) => {
   return (
     <article className={styles.entry}>
       <div className={styles.entryContent}>
-        {data.media_type === 'image' ? (
-          <img src={data.url} alt={data.title} />
+        {media_type === 'image' ? (
+          <img src={url} alt={title} />
         ) : (
           <div className={styles.videoContainer}>
             <iframe
-              src={data.url}
-              title={data.title}
+              src={url}
+              title={title}
               allow="accelerometer; autoplay; encrypted-media; fullscreen; geolocation; gyroscope; picture-in-picture"
             />
           </div>
         )}
-        <h3>{data.title}</h3>
+        <h3>{title}</h3>
         <p className={styles.date}>
-          {data.copyright ? `${data.copyright}: ${data.date}` : `${data.date}`}
+          {copyright ? `${copyright}: ${date}` : `${date}`}
         </p>
-        <p className={styles.explanation}>{data.explanation}</p>
+        <p className={styles.explanation}>{explanation}</p>
         <button
           type="button"
           onClick={handleClick}
