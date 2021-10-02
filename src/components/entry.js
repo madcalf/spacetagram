d1763bd4b64d305f1c8e752c039e9fc898a66f8f
D Dixon <d@themadcalf.com>
2021-10-02 16:04:54 -0700

    Fix setting initial liked state from local storage

--- src/components/entry.js
+++ src/components/entry.js
@@ -2,36 +2,21 @@ import React, { useState, useEffect } from 'react';
 import * as styles from '../styles/entry.module.css';
 import { storageAvailable } from '../utils/storage';
 
-/* Each item has the following
-copyright: " "
-date:	"2021-09-21"
-explanation:	"... The captured scene, however, is now frozen in time for all to enjoy."
-hdurl:	"https://apod.nasa.gov/apod/image/2109/SunSpotHill_Coy_2358.jpg"
-media_type:	"image"
-service_version:	"v1"
-title:	"Sun Spot Hill"
-url	"https://apod.nasa.gov/apod/image/2109/SunSpotHill_Coy_960.jpg"
-*/
-
 const Entry = ({ data }) => {
-  const [liked, setLiked] = useState(false);
-  const storage = window.localStorage;
   const { title, url, copyright, explanation, date, media_type } = data;
+  const [liked, setLiked] = useState(() => {
+    if (storageAvailable) {
+      const storedItemData = JSON.parse(localStorage.getItem(title));
+      return storedItemData?.liked ?? false;
+    }
+    return false;
+  });
 
-  // not sure yet how to handle the linter error since i only want this effect to run on mount
   useEffect(() => {
     if (storageAvailable) {
-      const stored = JSON.parse(storage.getItem(title));
-      setLiked(stored.liked);
-    } else {
-      setLiked(false);
+      localStorage.setItem(title, JSON.stringify({ liked: liked }));
     }
-  }, []);
-
-  // Also here, since i only want this effect to run when liked changes, not anything else.
-  useEffect(() => {
-    storage.setItem(title, JSON.stringify({ liked: liked }));
-  }, [liked]);
+  }, [liked, title]);
 
   const handleClick = () => {
     setLiked((liked) => !liked);
